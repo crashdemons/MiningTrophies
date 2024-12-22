545742a9ee3037e6d88251dd2265c5b3557f5138
crashdemons <crashenator@gmail.com>
2024-12-22 13:22:16 -0600

    doc update

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.github.crashdemons.miningtrophies"},{"l":"com.github.crashdemons.miningtrophies.events"},{"l":"com.github.crashdemons.miningtrophies.modifiers"}];updateSearchResults();
